28928bcb9b219df12466fcf637456f2a51113adb
Jesper Lund <jeslu246@student.liu.se>
2018-02-19 15:37:01 +0100

    refactor

--- main.m
+++ main.m
@@ -10,6 +10,12 @@ createDatabase('../database');
 image = imread('images/strykjarnet.jpg');
 
 [r, c, ~] = size(image);
+
+% Warn user if input image is too small
+if r < 128 || c < 128
+    disp('Warning: The image is too small and will be magnified, which will affect the resulting image.');
+end
+
 if r > c
   image_resized = imresize(image, (256/2) / r);
 elseif c > r
@@ -18,16 +24,13 @@ else
   image_resized = imresize(image, [(256/2), (256/2)]);
 end
 
-
 tic
 result = mosaic_reproduce(image_resized);
 result = imresize(result, [r, c]);
 result = imgaussfilt(result, 2);
 toc
+
 figure
 imshow(image);
 figure
 imshow(result);
-
-% The program should be independent of image size,
-% scale input image if necessary
